28a4f27bb625afac1a69a0599d253737f0b5470c
sarisim <sarina.simantov@mail.huji.ac.il>
2025-05-26 21:41:58 +0300

    day1_terrain

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -1,9 +1,36 @@
 package pepse;
 
 import danogl.GameManager;
+import danogl.GameObject;
+import danogl.collisions.Layer;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+import pepse.world.Block;
+import pepse.world.Terrain;
+
+import java.util.List;
 
 public class PepseGameManager extends GameManager {
+
+
+
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        GameObject sky = pepse.world.Sky.create(windowController.getWindowDimensions());
+        gameObjects().addGameObject(sky, Layer.BACKGROUND);
+
+        Terrain terrain =  new Terrain(windowController.getWindowDimensions(), 1234);
+        List<Block> blocks = terrain.createInRange(0, (int)windowController.getWindowDimensions().x());
+        for (Block block : blocks) {
+            gameObjects().addGameObject(block, Layer.STATIC_OBJECTS);
+        }
+    }
+
     public static void main(String[] args) {
+        new PepseGameManager().run();
 
     }
 }
---
+++ src/pepse/world/Block.java
@@ -0,0 +1,23 @@
+package pepse.world;
+
+import danogl.GameObject;
+import danogl.components.GameObjectPhysics;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+
+public class Block extends GameObject {
+    public static final int SIZE = 30;
+    /**
+     * Construct a new GameObject instance.
+     *
+     * @param topLeftCorner Position of the object, in window coordinates (pixels).
+     *                      Note that (0,0) is the top-left corner of the window.
+     * @param renderable    The renderable representing the object. Can be null, in which case
+     *                      the GameObject will not be rendered.
+     */
+    public Block(Vector2 topLeftCorner, Renderable renderable) {
+        super(topLeftCorner, Vector2.ONES.mult(SIZE), renderable);
+        physics().preventIntersectionsFromDirection(Vector2.ZERO);
+        physics().setMass(GameObjectPhysics.IMMOVABLE_MASS);
+    }
+}
